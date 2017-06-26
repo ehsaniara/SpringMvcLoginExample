93ae7fc684032fd36d584308aeec3123bf90fc51
ehsaniara <ehsaniara@gmail.com>
2017-06-26 04:25:21 -0700

    Spring Mvc Login Example

---
+++ src/main/java/com/exapot/config/SpringAdminConfig.java
@@ -0,0 +1,23 @@
+package com.exapot.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * From https://www.exapot.com
+ */
+@EnableWebMvc
+@Configuration
+@ComponentScan({"com.exapot.*"})
+public class SpringAdminConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+}
---
+++ src/main/java/com/exapot/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.exapot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * From https://www.exapot.com
+ */
+@Controller
+@RequestMapping("/")
+public class HelloWorldController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hi from Spring 4 MVC");
+        return "welcome";
+    }
+
+    @RequestMapping(value = "/hiAgain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("greeting", "Hi Again, from Spring 4 MVC");
+        return "welcome";
+    }
+}
