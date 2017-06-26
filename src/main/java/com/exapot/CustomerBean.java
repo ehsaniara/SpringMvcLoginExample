a424e1db512567b43196904c1d5956055650be9d
ehsaniara <ehsaniara@gmail.com>
2017-06-26 04:27:57 -0700

    Spring Mvc Login Example

---
+++ src/main/java/com/exapot/CustomerBean.java
@@ -0,0 +1,32 @@
+package com.exapot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.jsf.FacesContextUtils;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+
+/**
+ * From https://www.exapot.com
+ */
+@ManagedBean
+@SessionScoped
+public class CustomerBean implements Serializable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CustomerBean.class);
+
+    @PostConstruct
+    private void init() {
+        FacesContextUtils
+                .getRequiredWebApplicationContext(FacesContext.getCurrentInstance())
+                .getAutowireCapableBeanFactory().autowireBean(this);
+    }
+
+    public String loginCheck() {
+        return null;
+    }
+}
